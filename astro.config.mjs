64afed61826418df537a35086d77eaa65875cf9e
Moe <57568144+Root-Shells@users.noreply.github.com>
2023-11-27 02:09:14 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import rss from '@astrojs/rss';
 
 export default defineConfig({
   site: 'https://Root-Shells.github.io',
-})
+  integrations: [
+    react(),
+    sitemap(),
+    rss({
+      // Configure your RSS feed options here
+    }),
+  ],
+  markdown: {
+    // Configure your markdown options here
+  },
+});
